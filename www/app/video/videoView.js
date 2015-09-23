3fadb7ffa4fc816697053c985505c32085ebe871
gerhardboer <santifi#1987>
2015-09-23 20:04:30 +0200

    video stream properties to css (speed?), maybe test it

--- www/app/video/videoView.js
+++ www/app/video/videoView.js
@@ -2,13 +2,19 @@
     angular.module('GraDomo')
         .controller('videoView', ctrl);
 
-    ctrl.$inject = ['$interval', '$scope', 'videoService'];
+    ctrl.$inject = ['$timeout', '$scope', 'videoService'];
 
-    function ctrl($interval, $scope, videoService) {
+    function ctrl($timeout, $scope, videoService) {
         var vm = this;
 
         vm.streamUrl = '';
         vm.getStream = getStream;
+        vm.isAndroid = ionic.Platform.isAndroid();
+
+        vm.videoState = 'started';
+
+        $scope.$on('video-paused', handleVideoPaused);
+        $scope.$on('video-started', handleVideoStarted);
 
         $scope.$on('video-update', handleVideoUpdate);
         $scope.$on('$ionicView.afterEnter', beforeEnter);
@@ -25,6 +31,18 @@
 
         function onClose(evt) {
 
+        }
+
+        function handleVideoPaused() {
+            $timeout(function() {
+                vm.videoState = 'paused';
+            }, 0);
+        }
+
+        function handleVideoStarted() {
+            $timeout(function() {
+                vm.videoState = 'started';
+            }, 0);
         }
 
         function beforeEnter() {
